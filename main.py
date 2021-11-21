5069c70a593db789a30a91309fd22d19c352c038
Yoshio Kondo <yoskondo@yahoo-corp.jp>
2021-11-21 14:48:44 +0900

    Add Nand function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello')
+def main():
+    nand_test()
+
+
+def nand(x, y):
+    return not (x and y)
+
+
+def nand_test():
+    print(f'Actual [ x=0, y=0: {nand(0, 0)} ], Expected[ True ]')
+    print(f'Actual [ x=1, y=0: {nand(1, 0)} ], Expected[ True ]')
+    print(f'Actual [ x=0, y=1: {nand(0, 1)} ], Expected[ True ]')
+    print(f'Actual [ x=1, y=1: {nand(1, 1)} ], Expected[ False ]')
+
+
+main()
