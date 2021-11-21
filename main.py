406027c5937b3a57fc918e7438b5c2f4d4f3acfb
Yoshio Kondo <yoskondo@yahoo-corp.jp>
2021-11-21 15:34:38 +0900

    Add Not, Or, Xor

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+# Nand
 def nand(x, y):
     return not (x and y)
+
+
+# Not
+def not_func(x):
+    return nand(x, x)
+
+
+# Or
+def or_func(x, y):
+    if x == y:
+        return not_func(nand(x, y))
+
+    return nand(x, y)
+
+
+# Xor
+def xor(x, y):
+    if x != y:
+        return nand(x, y)
+    if or_func(x, y):
+        return nand(x, y)
+
+    return not_func(nand(x, y))
--- test_main.py
+++ test_main.py
@@ -9,3 +9,19 @@ class TestMain(TestCase):
         self.assertEqual(True, main.nand(1, 0))
         self.assertEqual(True, main.nand(0, 1))
         self.assertEqual(False, main.nand(1, 1))
+
+    def test_not_func(self):
+        self.assertEqual(True, main.not_func(0))
+        self.assertEqual(False, main.not_func(1))
+
+    def test_or_func(self):
+        self.assertEqual(False, main.or_func(0, 0))
+        self.assertEqual(True, main.or_func(0, 1))
+        self.assertEqual(True, main.or_func(1, 0))
+        self.assertEqual(True, main.or_func(1, 1))
+
+    def test_xor(self):
+        self.assertEqual(False, main.xor(0, 0))
+        self.assertEqual(True, main.xor(0, 1))
+        self.assertEqual(True, main.xor(1, 0))
+        self.assertEqual(False, main.xor(1, 1))
